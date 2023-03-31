ea39fcbd6de304fca81010df351e9619e9b29881
cmsmith890 <122848103+cmsmith890@users.noreply.github.com>
2023-03-30 23:31:16 -0400

    Update Sentimental Analysis.py

--- Sentimental Analysis.py
+++ Sentimental Analysis.py
@@ -1,5 +1,13 @@
 import nltk
-from nltk.corpus import movie_reviews
+import movie_reviews
+import csv
+
+
+with open('IMDB_dataset_320.000_reviews', mode="r") as csv_file: 
+    reader = csv.reader(r'C:\Users\cmsmi\Final_Project\IMDB_dataset_320.000_reviews.csv') 
+
+    for item in reader:
+        print(item)
 
 movie_reviews.words()
 len(movie_reviews.words())
@@ -8,28 +16,33 @@ nltk.FreqDist(movie_reviews.words())
 nltk.FreqDist(movie_reviews.words())['happy']
 nltk.FreqDist(movie_reviews.words()).most_common(15)
 movie_reviews.fileids()
-movie_reviews.fileids(‘pos’)
-movie_reviews.fileids(‘neg’)
+movie_reviews.fileids('pos')
+movie_reviews.fileids('neg')
 
-movie_reviews.words(‘neg/cv001_19502.txt’)
+movie_reviews.words('neg/textword_1.txt')
 
 all_words = nltk.FreqDist(movie_reviews.words())
 feature_vector = list(all_words)[:4000]
 feature = {}
-review = movie_reviews.words(‘neg/cv954_19932.txt’)
+review = movie_reviews.words('neg/textword_2.txt')
 for x in range(len(feature_vector)):
  feature[feature_vector[x]] = feature_vector[x] in review
- [x for x in feature_vector if feature[x] == True]
- document = [(movie_reviews.words(file_id),category) for file_id in movie_reviews.fileids() for category in movie_reviews.categories(file_id)]document
+ [x for x in feature_vector 
+  if feature[x] == True]
+ document = [(movie_reviews.words(file_id),category) 
+             for file_id in movie_reviews.fileids() 
+             for category in movie_reviews.categories(file_id)]
  
  find_feature(document[0][0])
- feature_sets = [(find_feature(word_list),category) for (word_list,category) in document]
+ feature_sets = [(find_feature(word_list),category) 
+                 for (word_list,category) in document]
  from nltk.classify.scikitlearn import SklearnClassifier
 from sklearn.svm import SVC
 from sklearn import model_selection
 train_set,test_set = model_selection.train_test_split(feature_sets,test_size = 0.25)
 print(len(train_set))
 print(len(test_set))
-model = SklearnClassifier(SVC(kernel = ‘linear’))
+model = SklearnClassifier(SVC(kernel = 'linear'))
 model.train(train_set)
-accuracy = nltk.classify.accuracy(model, test_set)print(‘SVC Accuracy : {}’.format(accuracy))
+accuracy = nltk.classify.accuracy(model, test_set)
+print('VC Accuracy : {}'.format(accuracy))
