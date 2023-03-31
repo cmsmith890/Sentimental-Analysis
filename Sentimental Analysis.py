d18814b66a804f5c5a03c4a85a417af7290f7c91
cmsmith890 <122848103+cmsmith890@users.noreply.github.com>
2023-03-31 00:36:34 -0400

    Update Sentimental Analysis.py

--- Sentimental Analysis.py
+++ Sentimental Analysis.py
@@ -1,10 +1,11 @@
 import nltk
 import movie_reviews
 import csv
+import pandas as pd
 
 
-with open('IMDB_dataset_320.000_reviews', mode="r") as csv_file: 
-    reader = csv.reader(r'C:\Users\cmsmi\Final_Project\IMDB_dataset_320.000_reviews.csv') 
+with open('IMDB_dataset_320.000_reviews.csv', mode="r") as csv_file: 
+    reader = csv.reader(csv_file) 
 
     for item in reader:
         print(item)
@@ -23,6 +24,7 @@ movie_reviews.words('neg/textword_1.txt')
 
 all_words = nltk.FreqDist(movie_reviews.words())
 feature_vector = list(all_words)[:4000]
+def find_feature(word_list):
 feature = {}
 review = movie_reviews.words('neg/textword_2.txt')
 for x in range(len(feature_vector)):
